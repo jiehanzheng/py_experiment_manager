7c676bf0ba71dbb95621e9a50f2d0e45372e8d6c
Jiehan Zheng <zheng@jiehan.org>
2013-10-04 16:40:46 -0400

    i guess the key is to make it the login shell then

--- server_adapters/ssh.py
+++ server_adapters/ssh.py
@@ -130,7 +130,7 @@ class SSHRunner(Runner):
 
     # the "python `which ...`" thing is a workaround to avoid using Columbia's old Python
     self.logger.info(self.hostname + " is training on " + folder_name + '...')
-    _, stdout, stderr = self._run_command('/bin/bash -c ' + quote('cd ' + self.working_directory + '/' + folder_name + '; ' + 'python `which train_and_test.py` ' + quote(svm_params)))
+    _, stdout, stderr = self._run_command('/bin/bash --login -c ' + quote('cd ' + self.working_directory + '/' + folder_name + '; ' + 'python `which train_and_test.py` ' + quote(svm_params)))
 
     json_result = stdout.read()
 
